836d647baae3b6ed6d7aee5266fa2407a6811869
Nabeeh Abusaleh <nabeeh@campus.technion.ac.il>
2020-12-19 13:36:43 +0200

    Added comments to date.c

--- date.c
+++ date.c
@@ -3,17 +3,18 @@
 #include <stdlib.h>
 #include "date.h"
 
-#define DATE_VALIDATION ((day >= 1 && day <= 30) && (month >= 1 && month <= 12)) // check validation for day, month and year
+//Checks if day and month are legal dates.
+#define DATE_VALIDATION ((day >= 1 && day <= 30) && (month >= 1 && month <= 12))
 
-struct Date_t 
-{
+//Date structure.
+struct Date_t{
     int day;
     int month;
     int year;
 };
 
-int dateToDays (Date date)//returns the total days in a specific date.
-{
+//Converts the given date to the total number of days in it and return that, it is useful in dateCompare function.
+int convertDateToDays (Date date){
     int total_days = 0;
     total_days = total_days + date->day;
     total_days = total_days + (date->month * 30);
@@ -22,16 +23,14 @@ int dateToDays (Date date)//returns the total days in a specific date.
     return total_days;
 }
 
-
-Date dateCreate(int day, int month, int year)//creates a new Date.
-{
+//creates a new Date and returns it.
+Date dateCreate(int day, int month, int year){
     if(!DATE_VALIDATION){
         return NULL;
     }
 
     Date date = malloc(sizeof(*date));
-    if(date == NULL)
-    {
+    if(date == NULL){
         return NULL;
     }
 
@@ -42,21 +41,19 @@ Date dateCreate(int day, int month, int year)//creates a new Date.
     return date;
 }
 
-void dateDestroy(Date date)//Destroys a Date.
-{
+//Destroys a Date.
+void dateDestroy(Date date){
     free(date);
 }
 
-Date dateCopy(Date date)//Copies the given date into another one and returns it.
-{
-    if(date == NULL)
-    {
+//Copies the given date into another one and returns it.
+Date dateCopy(Date date){
+    if(date == NULL){
         return NULL;
     }
 
     Date copied_date = malloc(sizeof(*copied_date));
-    if(copied_date == NULL)
-    {
+    if(copied_date == NULL){
         return NULL;
     }
 
@@ -67,10 +64,9 @@ Date dateCopy(Date date)//Copies the given date into another one and returns it.
     return copied_date;
 }
 
-bool dateGet(Date date, int* day, int* month, int* year)//Gets the day, month and year from the given date and puts them into a pointer for each one
-{
-    if (date == NULL || day == NULL || month == NULL || year == NULL)
-    {
+//Takes the day, month and year from the given date and puts them into a pointer for each one.
+bool dateGet(Date date, int* day, int* month, int* year){
+    if (date == NULL || day == NULL || month == NULL || year == NULL){
         return false;
     }
 
@@ -81,16 +77,16 @@ bool dateGet(Date date, int* day, int* month, int* year)//Gets the day, month an
     return true;
 }
 
-int dateCompare(Date date1, Date date2)//Compares between two dates and returns 1 if date2 is before date1 or -1 if date1 is before date2 or 0 if they are the same date
-{
-    if (date1 == NULL || date2 == NULL)
-    {
+//Compares between two dates and returns 1 if date2 is before date1 or -1 if date1 is before date2 or 0 if they are the same date
+int dateCompare(Date date1, Date date2){
+    if (date1 == NULL || date2 == NULL){
         return 0;
     }
     int date1_total_days, date2_total_days;
 
-    date1_total_days = dateToDays (date1);
-    date2_total_days = dateToDays (date2);
+    //Using converDateToDays is easier than checking manually by loops.
+    date1_total_days = convertDateToDays (date1);
+    date2_total_days = convertDateToDays (date2);
 
     if(date1_total_days < date2_total_days){
         return -1;
@@ -99,18 +95,14 @@ int dateCompare(Date date1, Date date2)//Compares between two dates and returns 
     }else{
         return 0;
     }
-    
 }
 
-void dateTick(Date date)//Increases the date by 1 day.
-{
-    if(date != NULL)
-    {
-        if(date->day < 30)
-        {
+//Increases the date by 1 day.
+void dateTick(Date date){
+    if(date != NULL){
+        if(date->day < 30){
             date->day++;
-        }else if(date->month < 12)
-        {
+        }else if(date->month < 12){
             date->day = 1;
             date->month++;
         }else{
@@ -118,7 +110,5 @@ void dateTick(Date date)//Increases the date by 1 day.
             date->month = 1;
             date->year++;
         }
-    
-        
     }
 }
